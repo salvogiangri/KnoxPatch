43e8079bc5d587245d1002876bfa3c0ee6dddabc
BlackMesa123 <giangrecosalvo9@gmail.com>
2023-05-17 14:28:53 +0200

    app: 0.5.2
    Signed-off-by: BlackMesa123 <giangrecosalvo9@gmail.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 
 val versionMajor = 0
 val versionMinor = 5
-val versionPatch = 1
+val versionPatch = 2
 
 val releaseStoreFile: String? by rootProject
 val releaseStorePassword: String? by rootProject
