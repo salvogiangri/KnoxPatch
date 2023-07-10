a2a8fff3ef8adfcbb1d4c027c37e48d576b00676
BlackMesa123 <giangrecosalvo9@gmail.com>
2023-07-10 22:47:59 +0200

    app: update gradle plug-in(s)
    Signed-off-by: BlackMesa123 <giangrecosalvo9@gmail.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,8 +7,8 @@ buildscript {
 
     dependencies {
         classpath("com.android.tools.build:gradle:8.0.2")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.22")
-        classpath("com.google.devtools.ksp:com.google.devtools.ksp.gradle.plugin:1.8.22-1.0.11")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.0")
+        classpath("com.google.devtools.ksp:com.google.devtools.ksp.gradle.plugin:1.9.0-1.0.11")
         classpath("dev.rikka.tools.refine:gradle-plugin:4.3.0")
     }
 }
