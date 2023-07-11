e681328aacf1f076acbcf4312c856e9237063055
BlackMesa123 <giangrecosalvo9@gmail.com>
2023-07-11 11:27:50 +0200

    stub: gradle: minor adjustments
    Signed-off-by: BlackMesa123 <giangrecosalvo9@gmail.com>

--- stub/build.gradle.kts
+++ stub/build.gradle.kts
@@ -16,7 +16,9 @@ android {
 }
 
 dependencies {
+    // HiddenApiRefinePlugin: https://github.com/RikkaApps/HiddenApiRefinePlugin
     annotationProcessor("dev.rikka.tools.refine:annotation-processor:4.3.0")
     compileOnly("dev.rikka.tools.refine:annotation:4.3.0")
+    // AndroidX: https://developer.android.com/jetpack/androidx/versions
     implementation("androidx.annotation:annotation:1.6.0")
 }
