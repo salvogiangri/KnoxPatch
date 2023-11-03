f274d9ffaec239f7514c40c8142322559b8ea315
BlackMesa123 <giangrecosalvo9@gmail.com>
2023-11-03 12:56:40 +0100

    app: 0.6.6
    Signed-off-by: BlackMesa123 <giangrecosalvo9@gmail.com>

--- buildSrc/src/main/java/Config.kt
+++ buildSrc/src/main/java/Config.kt
@@ -28,7 +28,7 @@ private var commitHash: String? = ""
 object Config {
     private const val VERSION_MAJOR = 0
     private const val VERSION_MINOR = 6
-    private const val VERSION_PATCH = 5
+    private const val VERSION_PATCH = 6
 
     val versionName: String
         get() {
