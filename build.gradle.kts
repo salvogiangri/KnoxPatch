db1385ac45ba733986ac7a724e942864a2b29d49
BlackMesa123 <giangrecosalvo9@gmail.com>
2023-12-11 16:21:05 +0100

    app: update gradle plug-in
    Signed-off-by: BlackMesa123 <giangrecosalvo9@gmail.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ buildscript {
     }
 
     dependencies {
-        classpath("com.android.tools.build:gradle:8.1.4")
+        classpath("com.android.tools.build:gradle:8.2.0")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.21")
         classpath("com.google.devtools.ksp:com.google.devtools.ksp.gradle.plugin:1.9.21-1.0.15")
         classpath("dev.rikka.tools.refine:gradle-plugin:4.4.0")
