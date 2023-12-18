82e0b64a2d7fe246140562757a76c2264ac13223
BlackMesa123 <giangrecosalvo9@gmail.com>
2023-12-18 11:04:24 +0100

    app: update gradle plug-in(s)
    Signed-off-by: BlackMesa123 <giangrecosalvo9@gmail.com>

--- stub/build.gradle.kts
+++ stub/build.gradle.kts
@@ -21,5 +21,5 @@ dependencies {
     annotationProcessor("dev.rikka.tools.refine:annotation-processor:4.4.0")
     compileOnly("dev.rikka.tools.refine:annotation:4.4.0")
     // AndroidX: https://developer.android.com/jetpack/androidx/versions
-    implementation("androidx.annotation:annotation:1.7.0")
+    implementation("androidx.annotation:annotation:1.7.1")
 }
